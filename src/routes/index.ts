07a74a93e09f4e41285167886cfaba21bf17d67a
irhasa <irhasalba19@gmail.com>
2024-11-06 13:16:39 +0700

    add middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,11 +13,11 @@ route.get("/test",(req,res) => {
 
 route.post("/users/register",UserController.registerUser)
 route.post("/users/login",UserController.loginUser)
-route.post("/user/profile/create",ProfileController.createProfile)
-route.put("/user/profile/update/:id",ProfileController.updateProfilee)
-route.post("/user/profile-content/create",ProfileController.uploadContent)
+route.post("/user/profile/create",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),ProfileController.createProfile)
+route.put("/user/profile/update/:id",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),ProfileController.updateProfilee)
+route.post("/user/profile-content/create",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),ProfileController.uploadContent)
 route.post("/user/match-up",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),MatchUpController.userMatchUp)
-route.post("/user/upgrade-premium",TransactionController.UpgradePremium)
+route.post("/user/upgrade-premium",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),TransactionController.UpgradePremium)
 route.get("/profile/all",(req,res,next) => AuthMiddleware.isAuthenticated(req,res,next),ProfileController.getAllProfile)
 
 
